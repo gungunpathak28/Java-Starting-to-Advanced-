2527865b461a600c7d0dfc9d22d812527e25c516
Gungun pathak <gungunpathak.2006@gmail.com>
2026-02-03 22:10:38 +0530

    Add files via upload

---
+++ AssignmentOperatorExample.java
@@ -0,0 +1,14 @@
+public class AssignmentOperatorExample {
+    public static void main(String[] args) {
+        int x = 10;
+        System.out.println(x);   
+        x += 5;
+        System.out.println(x);   
+        x -= 3;
+        System.out.println(x);  
+        x *= 2;
+        System.out.println(x);   
+        x /= 4;
+        System.out.println(x);   
+    }
+}
---
+++ JavaFirstProgram.java
@@ -0,0 +1,9 @@
+
+public class JavaFirstProgram {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+
+	}
+
+}
