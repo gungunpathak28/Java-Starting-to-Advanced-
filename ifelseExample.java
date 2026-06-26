9d21e4d7c69cd063958b36941b8c883cbf71b551
Gungun pathak <gungunpathak.2006@gmail.com>
2026-06-26 21:11:38 +0530

    Fix formatting issues in ifelseExample.java

--- ifelseExample.java
+++ ifelseExample.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 public class ifelseExample {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter a number: ");
         int num = sc.nextInt();
         
